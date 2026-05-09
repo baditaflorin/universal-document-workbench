b81a0d38ba0aca2a78c8d86668eef0722b751789
Florin Badita-Nistor <baditaflorin@gmail.com>
2026-05-09 19:17:42 +0300

    chore: publish phase 3 pages build

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "universal-document-workbench-v0.2.0";
+const CACHE_NAME = "universal-document-workbench-v0.3.0";
 const APP_SHELL = [
   "/universal-document-workbench/",
   "/universal-document-workbench/manifest.webmanifest",
